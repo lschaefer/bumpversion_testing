13f1bbc28e0a8903970aa253bff2064ef3e8ebca
Leigh Schaefer <schaeferleighc@gmail.com>
2020-10-16 12:13:58 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name='bumpversion_testing',
+    version='0.0.1',
+    description='dummy repo for bumpversion testing',
+    author='lschaefer',
+    license='',
+)
