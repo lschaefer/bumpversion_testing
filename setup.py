fc87bc82d2656ef46b2c95628367bf5bf8ec19a7
Leigh Schaefer <schaeferleighc@gmail.com>
2020-10-16 12:23:56 -0400

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='bumpversion_testing',
-    version='0.0.1',
+    version='0.0.2',
     description='dummy repo for bumpversion testing',
     author='lschaefer',
     license='',
