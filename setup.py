8aa89020c10dc4f776876e65cb55af3154ae90a4
GitHub Action <action@github.com>
2020-11-02 16:46:13 +0000

    Bump version: 0.0.11 → 0.0.12

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='bumpversion_testing',
-    version='0.0.11',
+    version='0.0.12',
     description='dummy repo for bumpversion testing',
     author='lschaefer',
     license='',
